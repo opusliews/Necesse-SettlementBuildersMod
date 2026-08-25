8104ca7f3fabb7725f064b2b68a6ff07eb175455
Opus <opusliews@gmail.com>
2026-08-25 17:28:42 -0300

    First commit: Initial mod running test

---
+++ src/main/java/SettlementBuilders.java
@@ -0,0 +1,8 @@
+import necesse.engine.modLoader.annotations.ModEntry;
+
+@ModEntry
+public class SettlementBuilders {
+    public void init() {
+        System.out.println("Poop: Mod is running!");
+    }
+}
