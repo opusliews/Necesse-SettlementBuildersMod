f3c8d543e594736997c0d4e50240c07da9a13d98
Opus <opusliews@gmail.com>
2026-09-02 00:04:00 -0300

    Refactoring PacketHardcoreDamageSetting

--- src/main/java/opus/network/PacketRequestBlueprintAreas.java
+++ src/main/java/opus/network/PacketRequestBlueprintAreas.java
@@ -6,7 +6,6 @@ import necesse.engine.network.server.Server;
 import necesse.engine.network.server.ServerClient;
 import necesse.level.maps.Level;
 import opus.blueprint.BlueprintAreaManager;
-import opus.damage.HardcoreDamage;
 
 public class PacketRequestBlueprintAreas extends Packet {
 	public PacketRequestBlueprintAreas() {
@@ -30,7 +29,6 @@ public class PacketRequestBlueprintAreas extends Packet {
 
 		BlueprintAreaManager manager = BlueprintAreaManager.get(level);
 
-		client.sendPacket(new PacketHardcoreDamageSetting(HardcoreDamage.isServerEnabled()));
 		client.sendPacket(
 			new PacketSyncBlueprintAreas(manager)
 		);
